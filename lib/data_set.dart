3e37a5330f52fdd07eff843442d1da6d841946e2
Adedoyin <isradeoluwa@gmail.com>
2022-02-09 17:02:47 +0100

    A few data set added

---
+++ lib/data_set.dart
@@ -0,0 +1,313 @@
+// DATA SET FOR THE MEALS
+
+const Map<String, dynamic> dataSET = {
+  'meal': [
+    // BREAKFASTS
+    {
+      'name': 'Pap',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 250,
+      'menu': {'name': 'Breakfast', 'image': null}
+    },
+    {
+      'name': 'Akara',
+      'image': null,
+      'rating': 4.4,
+      'detail': null,
+      'price': 100,
+      'menu': {'name': 'Breakfast', 'image': null}
+    },
+    {
+      'name': 'Custard',
+      'image': null,
+      'rating': 4.4,
+      'detail': null,
+      'price': 300,
+      'menu': {'name': 'Breakfast', 'image': null}
+    },
+    {
+      'name': 'Boiled Yam',
+      'image': null,
+      'rating': 4.3,
+      'detail': null,
+      'price': 700,
+      'menu': {'name': 'Breakfast', 'image': null}
+    },
+    {
+      'name': 'Fried Yam',
+      'image': null,
+      'rating': 4.4,
+      'detail': null,
+      'price': 750,
+      'menu': {'name': 'Breakfast', 'image': null}
+    },
+    {
+      'name': 'Boiled Plantain',
+      'image': null,
+      'rating': 4.4,
+      'detail': null,
+      'price': 550,
+      'menu': {'name': 'Breakfast', 'image': null}
+    },
+    {
+      'name': 'Toast Bread',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 200,
+      'menu': {'name': 'Breakfast', 'image': null}
+    },
+
+    // MAIN DISH
+    {
+      'name': 'Village Rice',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 700,
+      'menu': {'name': 'Main Dish', 'image': null}
+    },
+    {
+      'name': 'Jollof Rice',
+      'image': null,
+      'rating': 4.7,
+      'detail': null,
+      'price': 750,
+      'menu': {'name': 'Main Dish', 'image': null}
+    },
+    {
+      'name': 'Spaghetti',
+      'image': null,
+      'rating': 4.7,
+      'detail': null,
+      'price': 500,
+      'menu': {'name': 'Main Dish', 'image': null}
+    },
+    {
+      'name': 'White Rice',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 700,
+      'menu': {'name': 'Main Dish', 'image': null}
+    },
+    {
+      'name': 'Coconut Rice',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 750,
+      'menu': {'name': 'Main Dish', 'image': null}
+    },
+    {
+      'name': 'Fried Rice',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 700,
+      'menu': {'name': 'Main Dish', 'image': null}
+    },
+    {
+      'name': 'Rice & Beans',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 700,
+      'menu': {'name': 'Main Dish', 'image': null}
+    },
+
+    // EXTRAS
+    {
+      'name': 'Plantain (Dodo)',
+      'image': null,
+      'rating': 4.7,
+      'detail': null,
+      'price': 50,
+      'menu': {'name': 'Extras', 'image': null}
+    },
+    {
+      'name': 'Moi Moi',
+      'image': null,
+      'rating': 4.6,
+      'detail': null,
+      'price': 150,
+      'menu': {'name': 'Extras', 'image': null}
+    },
+    {
+      'name': 'Boiled Egg',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 150,
+      'menu': {'name': 'Extras', 'image': null}
+    },
+    {
+      'name': 'Fried Egg',
+      'image': null,
+      'rating': 4.6,
+      'detail': null,
+      'price': 200,
+      'menu': {'name': 'Extras', 'image': null}
+    },
+    {
+      'name': 'Salad',
+      'image': null,
+      'rating': 4.6,
+      'detail': null,
+      'price': 300,
+      'menu': {'name': 'Extras', 'image': null}
+    },
+
+    // SOUPS
+    {
+      'name': 'Egusi',
+      'image': null,
+      'rating': 4.7,
+      'detail': null,
+      'price': 200,
+      'menu': {'name': 'Soups', 'image': null}
+    },
+    {
+      'name': 'Afang',
+      'image': null,
+      'rating': 4.6,
+      'detail': null,
+      'price': 150,
+      'menu': {'name': 'Soups', 'image': null}
+    },
+    {
+      'name': 'Edikaikong',
+      'image': null,
+      'rating': 4.7,
+      'detail': null,
+      'price': 200,
+      'menu': {'name': 'Soups', 'image': null}
+    },
+    {
+      'name': 'Ofada Sauce',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 100,
+      'menu': {'name': 'Soups', 'image': null}
+    },
+    {
+      'name': 'Efo riro',
+      'image': null,
+      'rating': 4.7,
+      'detail': null,
+      'price': 200,
+      'menu': {'name': 'Soups', 'image': null}
+    },
+    {
+      'name': 'Ewedu',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 100,
+      'menu': {'name': 'Soups', 'image': null}
+    },
+
+    // SWALLOWS
+    {
+      'name': 'Eba',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 150,
+      'menu': {'name': 'Swallows', 'image': null}
+    },
+    {
+      'name': 'Amala',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 150,
+      'menu': {'name': 'Swallows', 'image': null}
+    },
+    {
+      'name': 'Semo',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 150,
+      'menu': {'name': 'Swallows', 'image': null}
+    },
+    {
+      'name': 'Wheat',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 150,
+      'menu': {'name': 'Swallows', 'image': null}
+    },
+
+    // PROTEINS
+    {
+      'name': 'Panla Fish',
+      'image': null,
+      'rating': 4.6,
+      'detail': null,
+      'price': 250,
+      'menu': {'name': 'Proteins', 'image': null}
+    },
+    {
+      'name': 'Fried Titus',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 250,
+      'menu': {'name': 'Proteins', 'image': null}
+    },
+    {
+      'name': 'Smoked Fish',
+      'image': null,
+      'rating': 4.6,
+      'detail': null,
+      'price': 250,
+      'menu': {'name': 'Proteins', 'image': null}
+    },
+    {
+      'name': 'Panla Fish',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 250,
+      'menu': {'name': 'Proteins', 'image': null}
+    },
+    {
+      'name': 'Beef',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 250,
+      'menu': {'name': 'Proteins', 'image': null}
+    },
+    {
+      'name': 'Assorted',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 250,
+      'menu': {'name': 'Proteins', 'image': null}
+    },
+    {
+      'name': 'Ogunfe (Goat Meat)',
+      'image': null,
+      'rating': 4.8,
+      'detail': null,
+      'price': 500,
+      'menu': {'name': 'Proteins', 'image': null}
+    },
+    {
+      'name': 'Grilled Chicken',
+      'image': null,
+      'rating': 4.5,
+      'detail': null,
+      'price': 250,
+      'menu': {'name': 'Proteins', 'image': null}
+    },
+  ]
+};
