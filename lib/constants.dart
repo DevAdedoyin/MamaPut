22838dc6509975b15ab08426d16d6df2842918c8
Adedoyin <isradeoluwa@gmail.com>
2022-02-15 18:02:43 +0100

    Color code added

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
 class Constants {
+  // IMAGES LOCATION
   static const String menuImages = 'assets/images/menu/';
   static const String mealImages = 'assets/images/meals/';
 
+  // DETAILS
   static const String details =
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+
+  // COLORS
+  static const Color primaryColor = Color.fromRGBO(42, 44, 65, 1);
+  static const Color secondaryColor = Color.fromRGBO(255, 39, 76, 1);
+  static const Color tertiaryColor = Color.fromRGBO(253, 191, 80, 1);
+  static const Color backgroundColor = Color.fromRGBO(244, 244, 248, 1);
 }
