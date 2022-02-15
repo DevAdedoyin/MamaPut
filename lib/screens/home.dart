a2df5742e23fa3bff4da147066f146e7d9470504
Adedoyin <isradeoluwa@gmail.com>
2022-02-15 18:04:39 +0100

    App bar title

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:mama_put/constants.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: RichText(text: TextSpan(children: [
-        TextSpan(text: 'Mama'),
-        TextSpan(text: 'Put'),
-      ])),),
+      appBar: AppBar(
+        title: RichText(
+            text: const TextSpan(children: [
+          TextSpan(
+              text: 'Mama', style: TextStyle(color: Constants.primaryColor)),
+          TextSpan(
+              text: 'Put', style: TextStyle(color: Constants.secondaryColor)),
+        ])),
+      ),
     );
   }
 }
